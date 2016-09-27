743efcba012fdd6dfa523b5bfbb7976beca26001
Simhachalamnaidu.S <s@gmail.com>
2016-09-27 15:21:43 +0530

    testing main fn merge

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-int main()
+static int var;
+volatile int count;
+
+void add(int,int);
+
+
+
+
+
+
+
+
+
+
+void add(int a,int b)
 {
-	int x,y,z;
-=======
-#define NUM 1024
-#define SET(num,bit) {num |= bit}
-
-int main()
-{
-	int c, num = 10, bit_pos = 2;
-
-	c = SET(num, bit_pos);
-
-	printf("%d \t %X\r\n", c, c);
-
->>>>>>> source
-	return 0;
+	int c;
+	c = a+b;
 }
