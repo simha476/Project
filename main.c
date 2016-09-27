c800783ea5dcdbf003a145d63be7bc7e710c554c
Simhachalamnaidu.S <s@gmail.com>
2016-09-27 15:30:38 +0530

    changes

--- main.c
+++ main.c
@@ -5,19 +5,25 @@ volatile int count;
 
 void add(int,int);
 
+#define NUM 1024
+#define SET(num,bit) {num |= bit}
 
+int main()
+{
+	int c, num = 10, bit_pos = 2;
 
+	c = SET(num, bit_pos);
 
+	add(5,10);
+	printf("%d \t %X\r\n", c, c);
 
-
+	return 0;
+}
 <<<<<<< HEAD
 =======
 	add(5,10);
 	printf("%d \t %X\r\n", c, c);
 >>>>>>> source
-
-
-
 
 void add(int a,int b)
 {
